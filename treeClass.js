1c36b5110ab6d35918e4159994a8311b841813ac
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-01 15:36:12 +0200

    Create insert() method

--- treeClass.js
+++ treeClass.js
@@ -1,3 +1,4 @@
+import { Node } from "./nodeClass.js";
 import { sort } from "./mergeSort.js";
 import { createBST } from "./bst.js";
 import { prettyPrint } from "./printBst.js";
@@ -13,6 +14,36 @@ class Tree {
     let sortedArray = sort(array);
     this.root = createBST(sortedArray);
     prettyPrint(this.root);
+  }
+
+  insert(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+      prettyPrint(this.root);
+      return;
+    }
+
+    let currNode = this.root;
+    while (currNode) {
+      if (currNode.data > value) {
+        if (!currNode.left) {
+          currNode.left = new Node(value);
+          prettyPrint(this.root);
+          return;
+        }
+        currNode = currNode.left;
+      } else if (currNode.data < value) {
+        if (!currNode.right) {
+          currNode.right = new Node(value);
+          prettyPrint(this.root);
+          return;
+        }
+        currNode = currNode.right;
+      } else {
+        prettyPrint(this.root);
+        return;
+      }
+    }
   }
 }
 
