7b66758b31c6c14cc5149e290f2f2c7eb184be86
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-01 14:20:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import { Tree } from "./treeClass.js";
 
-const tree = new Tree([5, 4, 3, 2, 1]);
-console.log(tree.buildTree(tree.array));
+const tree = new Tree([5, 4, 3, 2, 1, 9, 15, 14, 22, 88, 99]);
+tree.buildTree(tree.array);
