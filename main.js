c1c8907a194fd130fb73348a3288a1f206cd1118
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-02 15:25:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,3 @@ import { Tree } from "./treeClass.js";
 
 const tree = new Tree([5, 4, 3, 2, 1, 9, 15, 14, 22, 88, 99, 100]);
 tree.buildTree(tree.array);
-console.log();
-console.log(tree.isBalanced());
