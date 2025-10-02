74098fad75cff17dddd6a61c9a4f36c1cd0bc802
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-02 10:53:34 +0200

    Test the levelOrderForEach() method

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ import { Tree } from "./treeClass.js";
 
 const tree = new Tree([5, 4, 3, 2, 1, 9, 15, 14, 22, 88, 99]);
 tree.buildTree(tree.array);
+console.log();
+console.log();
+tree.levelOrderForEach((node) => {
+  console.log(node.data);
+});
