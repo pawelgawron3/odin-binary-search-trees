cb22572167c600a7eae878ee6bf494de8b32934c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-02 14:26:57 +0200

    Test the isBalanced() method

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 import { Tree } from "./treeClass.js";
 
-const tree = new Tree([5, 4, 3, 2, 1, 9, 15, 14, 22, 88, 99]);
+const tree = new Tree([5, 4, 3, 2, 1, 9, 15, 14, 22, 88, 99, 100]);
 tree.buildTree(tree.array);
 console.log();
-console.log();
-tree.levelOrderForEach((node) => {
-  console.log(node.data);
-});
-console.log();
-console.log("Preorder DFS");
-tree.preOrderForEach((node) => {
-  console.log(node.data);
-});
-console.log();
-console.log("Inorder DFS");
-tree.inOrderForEach((node) => {
-  console.log(node.data);
-});
-console.log();
-console.log("Postorder DFS");
-tree.postOrderForEach((node) => {
-  console.log(node.data);
-});
+console.log(tree.isBalanced());
